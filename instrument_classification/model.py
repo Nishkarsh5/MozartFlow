7c249b72ff2f7acc754cf096288dc89933da4c89
Ankit Solanki <ankit03june@gmail.com>
2018-05-27 17:41:39 +0530

    ADD: argparse module variables with initialization of logging.basicConfig()

--- instrument_classification/model.py
+++ instrument_classification/model.py
@@ -1,18 +1,21 @@
+import logging
+import argparse
 import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
 from collections import namedtuple
-
+from sklearn.neighbors import KNeighborsClassifier
 
 class Model:
-
     def __init__(self):
         with open('../config.yml', 'r') as outfile:
             try:
-                read_yml = yaml.load(outfile)
+                self.read_yml = yaml.load(outfile)
             except yaml.YAMLError as error:
                 print(error)
-        with open(read_yml['_dataset']/read_yml['_dataFolderNames'], 'r') as outfile:
+        with open(self.read_yml['_dataset']/self.read_yml['_dataFolderNames']+".json", 'r') as outfile:
             self.dataset_names = json.load(outfile)
+
+    self.logger = logging.getLogger()
+
 
     def model(self, n_neightbors):
         # consider we have labeled data here, named, data
@@ -25,10 +28,12 @@ class Model:
         datatuple = unpack_data(data)
         self.KNN.fit(datatuple.features, datatuple.labels)
 
+
     def test(self, data):
         datatuple = unpack_data(data)
         predicted = self.KNN.predict(datatuple.features)
         print('accuracy:', self.KNN.score(datatuple.features, datatuple.labels))
+
 
     def unpack_data(self, data):        
         filenames = np.array(map(lambda n: n[0], data))
@@ -37,3 +42,13 @@ class Model:
 
         Tup = namedtuple('Tup', 'filenames features labels')
         return Tup(filenames = filenames, features = features, labels = labels)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='MozartFlow: Observing the flow of music.')
+
+    parser.add_argument('-k', '-knn', help='K in K-nearest neighbours algorithm', default=5)
+    parser.add_argument('-ll', '-loglevel', help='Set the logging level', type=str, choices=['DEBUG','INFO','WARNING','ERROR','CRITICAL'])
+
+    args = parser.parse_args()
+    logging.basicConfig(level=args.loglevel)
