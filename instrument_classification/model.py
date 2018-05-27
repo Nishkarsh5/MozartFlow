6118299547445eac9968dd782754628bf099044d
Ankit Solanki <ankit03june@gmail.com>
2018-05-27 22:29:56 +0530

    UPADTE: model class completed with testing functions and loglevel logger

--- instrument_classification/model.py
+++ instrument_classification/model.py
@@ -1,3 +1,4 @@
+import librosa
 import logging
 import argparse
 import numpy as np
@@ -32,6 +33,7 @@ class Model:
         cl = classify.Classify(self.loglevel)
         data = cl.get_dataset()
 
+        self.loglevel.info('[*] Using K-nearest neighbour algorith ...')
         self.KNN = KNeighborsClassifier(n_neightbors = self.knn)
         train_and_test(data)
 
@@ -42,33 +44,62 @@ class Model:
         np.random.shuffle(data)
         datatuple = unpack_data(data)
 
-        self.loglevel('[*] 75-25 partition of datasets ...')
+        self.loglevel.info('[*] 75-25 partition of datasets ...')
         train_features, test_features, train_labels, test_labels = train_test_split(datatuple.features, datatuple.labels, random_state=0)
         self.loglevel.info('[*] Training started with 75% dataset ...')
 
         self.KNN.fit(train_features, train_labels)
 
         self.loglevel.info('[*] Testing started with 25% dataset ...')
-        print('/---------------Accuracy------------/') 
+        print('/---------------Accuracy----------------/') 
         
         accuracy = self.KNN.score(train_features, train_labels)
         print('Test set accuracy','{:.2f}'.format(accuracy*100), '%')
+
+        if accuracy < 0.40:
+            print('[-.-!] Thanks for tryin\' but this machine ain\'t learning.')
 
         return True
 
 
     def train(self, data):
+        self.loglevel('[.] Training with whole dataset ...')
+        
         datatuple = unpack_data(data)
         self.KNN.fit(datatuple.features, datatuple.labels)
 
 
-    def test(self, data):
-        datatuple = unpack_data(data)
+    def test(self, filepath):
+        try:
+            DTFTarray, sampling_rate = librosa.load(filepath)
+        except Exception as e:
+            self.loglevel.error('[/!/] Librosa loading test file failed.')
+
+        begin_silence = classify.get_silence(DTFTarray)
+        end_silence = classify.get_silence(np.flipud(DTFTarray))
+
+        self.loglevel.info('[.] Trimming the audio ...')
+
+        DTFTarray_trimmed = DTFTarray[begin_silence: (len(DTFTarray) - end_silence)]
+
+        mfccs = librosa.feature.mfccs(y=DTFTarray_trimmed, sr=sampling_rate)
+        average = np.mean(mfccs, axis=1)
+        
+        features = average.reshape(20)
+
+        self.loglevel.info('[.] Predicting with the audio features ...')
         predicted = self.KNN.predict(datatuple.features)
-        print('accuracy:', self.KNN.score(datatuple.features, datatuple.labels))
+        
+        inv_dataset_names = {value: key for key, value in self.dataset_names.items()}
+
+        label_predicted = inv_dataset_names[predicted]
+        print('[*] Prediction: The audio relates to ', label_predicted)
+
+        return True
 
 
     def unpack_data(self, data):        
+        
         filenames = np.array(map(lambda n: n[0], data))
         features = np.array(map(lambda n: n[1], data))
         labels = np.array(map(lambda n: n[0], data))
