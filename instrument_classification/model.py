57bcd8d73c2c11d800c55d65ed1098db069eefb3
Ankit Solanki <ankit03june@gmail.com>
2018-05-27 21:48:03 +0530

    EDIT: wrote training and testing function and started loglevel logging

--- instrument_classification/model.py
+++ instrument_classification/model.py
@@ -1,35 +1,60 @@
-import classify
-
 import logging
 import argparse
 import numpy as np
 
+import classify
+
 from collections import namedtuple
 from sklearn.neighbors import KNeighborsClassifier
-
+from sklearn.model_selection import train_test_split
 
 class Model:
     def __init__(self, knn, loglevel):
         self.knn = knn
         self.loglevel = loglevel
 
-        with open('../config.yml', 'r') as outfile:
+        with open('../_config.yml', 'r') as outfile:
             try:
                 self.read_yml = yaml.load(outfile)
 
             except yaml.YAMLError as error:
-                self.loglevel.error('[!] _config.yml file not found:', error)
+                self.loglevel.error('[/!/] _config.yml file not found:', error)
         
         with open(self.read_yml['_dataset']/self.read_yml['_dataFolderNames']+".json", 'r') as outfile:
             try:
                 self.dataset_names = json.load(outfile)
 
             except FileNotFoundError:
-                self.loglevel.error('[!] dataset labels not found')
+                self.loglevel.error('[/!/] dataset labels not found')
 
     def model(self):
+        self.loglevel.info('[*] Starting processing of dataset ...')
+        cl = classify.Classify(self.loglevel)
+        data = cl.get_dataset()
+
         self.KNN = KNeighborsClassifier(n_neightbors = self.knn)
         train_and_test(data)
+
+        return True
+
+
+    def train_and_test(self, data):
+        np.random.shuffle(data)
+        datatuple = unpack_data(data)
+
+        self.loglevel('[*] 75-25 partition of datasets ...')
+        train_features, test_features, train_labels, test_labels = train_test_split(datatuple.features, datatuple.labels, random_state=0)
+        self.loglevel.info('[*] Training started with 75% dataset ...')
+
+        self.KNN.fit(train_features, train_labels)
+
+        self.loglevel.info('[*] Testing started with 25% dataset ...')
+        print('/---------------Accuracy------------/') 
+        
+        accuracy = self.KNN.score(train_features, train_labels)
+        print('Test set accuracy','{:.2f}'.format(accuracy*100), '%')
+
+        return True
 
 
     def train(self, data):
@@ -64,3 +89,10 @@ def main():
     logging.basicConfig(level=args.loglevel)
 
     model = Model(args.knn, args.loglevel)
+
+    print('----/---- Created by ----/----')
+    for creator in model.read_yml['creator']:
+        print('Lord', creator)
+
+if __name__=="__main__":
+    main()
