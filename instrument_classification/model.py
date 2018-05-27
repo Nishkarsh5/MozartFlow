624770013399860314250ee595390970d4b8c973
Ankit Solanki <ankit03june@gmail.com>
2018-05-27 17:54:21 +0530

    UPDATE: initializing logging variables and plotted apt values

--- instrument_classification/model.py
+++ instrument_classification/model.py
@@ -1,26 +1,34 @@
+import classify
+
 import logging
 import argparse
 import numpy as np
+
 from collections import namedtuple
 from sklearn.neighbors import KNeighborsClassifier
 
+
 class Model:
-    def __init__(self):
+    def __init__(self, knn, loglevel):
+        self.knn = knn
+        self.loglevel = loglevel
+
         with open('../config.yml', 'r') as outfile:
             try:
                 self.read_yml = yaml.load(outfile)
+
             except yaml.YAMLError as error:
-                print(error)
+                self.loglevel.error('[!] _config.yml file not found:', error)
+        
         with open(self.read_yml['_dataset']/self.read_yml['_dataFolderNames']+".json", 'r') as outfile:
-            self.dataset_names = json.load(outfile)
+            try:
+                self.dataset_names = json.load(outfile)
 
-    self.logger = logging.getLogger()
+            except FileNotFoundError:
+                self.loglevel.error('[!] dataset labels not found')
 
-
-    def model(self, n_neightbors):
-        # consider we have labeled data here, named, data
-
-        self.KNN = KNeighborsClassifier(n_neightbors = n_neightbors)
+    def model(self):
+        self.KNN = KNeighborsClassifier(n_neightbors = self.knn)
         train_and_test(data)
 
 
@@ -45,6 +53,8 @@ class Model:
 
 
 def main():
+    self.logger = logging.getLogger()
+
     parser = argparse.ArgumentParser(description='MozartFlow: Observing the flow of music.')
 
     parser.add_argument('-k', '-knn', help='K in K-nearest neighbours algorithm', default=5)
@@ -52,3 +62,5 @@ def main():
 
     args = parser.parse_args()
     logging.basicConfig(level=args.loglevel)
+
+    model = Model(args.knn, args.loglevel)
