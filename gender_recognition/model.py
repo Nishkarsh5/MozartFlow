a2b3daa9df9b7e93a30b5130ab903f0fb3864786
Ankit Solanki <ankit03june@gmail.com>
2018-05-29 22:55:45 +0530

    [UPDATE] now with argparse and much script friendly

--- gender_recognition/model.py
+++ gender_recognition/model.py
@@ -11,11 +11,11 @@ logger = logging.getLogger()
 
 
 class Model:
-    def __init__(self, no_DTrees=5):
+    def __init__(self, no_DTrees):
         self.DTrees = no_DTrees
 
     def model(self):
-        logger.info('[*] Processing dataset ...')
+        logger.info('[*] Processing dataset ...\n')
         datasetdict = preprocessing.ProcessDataset().process_dataset()
 
         logger.info('[*] Preparing model for classification ...')
@@ -23,24 +23,32 @@ class Model:
         self.forest = RandomForestClassifier(n_estimators=self.DTrees, random_state=42)
         logger.infor('[.] Model formed' )
 
-    def train(self):
+        self.train()
 
+    def train(self):
 
         self.forest.fit(self.X_train, self.y_train)
 
+        logger.info('[.]-----------/-------------Accuracy-------------/-----------')
         self.accuracy = self.forest.score(self.X_test, self.y_test)*100
         logger.info('\n[*] Accuracy on training set: {:.3f} %'.format(self.forest.score(self.X_train, self.y_train)*100))
         logger.info('[*] Accuracy on test set: {:.3f} %\n'.format(accuracy))
 
+        return True
+
     def predict(self, filepath):
+
+        Pfeatures = self.get_audiofeatures(filepath)
 
         predict = self.forest.predict(Pfeatures)
         print('[*] Predicted gender:', predict, 'with accuracy {:.3f} %'.format(self.accuracy))
 
+        return True
+
     def predictnow(self):
         recog = sr.Recognizer()
         
-        logger.info('[!] Please speak for a few seconds now ...')
+        logger.info('[!] Please speak for a few seconds now ...\n')
         logger.info('[.] In case you do know what to say, say this:')
         logger.info('[.] The only way to live in this unfree world is to be so absoutely free that your very existence is an act of rebellion.')
 
@@ -53,9 +61,31 @@ class Model:
         except sr.RequestError as e:
             logger.error('[/!/] Could not request results: {0}'.format(e))
 
+        return True
 
-if __name__ == "__main__":
+    def get_audiofeatures(self, filepath):
+
+        Pfeatures = []
+
+        return Pfeatures
+
+if __name__ == "__main__":    
+    parser = argparse.ArgumentParser(description='Gender Voice Recognition')
+
+    parser.add_argument('-t', '--trees', help='No of decision trees (default=5)', default=5)
+    parser.add_argument('-p', '--predict', help='Give a filepath and get prediction', type=str, default='')
+    parser.add_argument('-P', '--predictnow', help='Start speaking and get prediction', default=False, action="store_true")
+    parser.add_argument('-L', '--log', help='Set the logging level', type=str, choices=['DEBUG','INFO','WARNING','ERROR','CRITICAL'])
         
-    logging.basicConfig(level=args.loglevel)
+    args = parser.parse_args()
+    logging.basicConfig(level=args.log)
 
-    m = Model().model()
+    mod = Model(args.trees)
+
+    if args.predictnow:
+        mod.predictnow()
+    elif args.predict is not '':
+        mod.predict(args.predict)
+
+        
+
